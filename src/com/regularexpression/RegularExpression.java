da2cb50abc8c4e634b2e69dae7220311f2722158
Yash-y-ash <wardhan696@gmail.com>
2021-03-29 22:20:12 +0530

    Added:welcome message

---
+++ src/com/regularexpression/RegularExpression.java
@@ -0,0 +1,10 @@
+package com.regularexpression;
+
+public class RegularExpression {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome to regular expression problem");
+	}
+
+}
