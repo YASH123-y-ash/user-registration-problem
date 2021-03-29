d45254b7a79e73fec59538da1bfd20245645a61b
Yash-y-ash <wardhan696@gmail.com>
2021-03-29 23:44:38 +0530

    Added:regular expression for checking user first name

--- src/com/regularexpression/RegularExpression.java
+++ src/com/regularexpression/RegularExpression.java
@@ -1,10 +1,25 @@
 package com.regularexpression;
 
+import java.util.*;
+import java.util.regex.*;
 public class RegularExpression {
 
-	public static void main(String[] args) {
-		
-		System.out.println("welcome to regular expression problem");
+	public static boolean firstName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name: ");
+		String first = sc.next();
+		// Using REGEX
+		if (Pattern.matches("^[A-Z][aA-zZ]{2,}", first)) {
+			System.out.println("First Name valid");
+			return true;
+		} else {
+			System.out.println("First Name invalid");
+			return firstName();
+		}
 	}
 
+	public static void main(String[] args) {
+		System.out.println("Welcome to user registration");
+		firstName();
+	}
 }
